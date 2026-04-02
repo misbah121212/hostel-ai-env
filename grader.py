5068bf166e42f4ef9917f25625fb6928fc2b4c2e
Misbah <misbasikandar7@gmail.com>
2026-04-02 23:22:53 +0530

    Add files via upload

--- grader.py
+++ grader.py
@@ -1,4 +1,24 @@
-def grade(predicted, actual):
-    if predicted == actual:
-        return 1.0
-    return 0.0
+def evaluate(task, action):
+    try:
+        # EASY
+        if task["type"] == "easy":
+            return action.get("category") == task["category"]
+
+        # MEDIUM
+        elif task["type"] == "medium":
+            return (
+                action.get("category") == task["category"]
+                and action.get("priority") == task["priority"]
+            )
+
+        # HARD
+        elif task["type"] == "hard":
+            return (
+                set(action.get("categories", [])) == set(task["category"])
+                and action.get("priority") == task["priority"]
+            )
+
+    except:
+        return False
+
+    return False
--- tasks.py
+++ tasks.py
@@ -1,42 +1,25 @@
-TASKS = [
+import random
 
-    # EASY
-    {
-        "type": "easy",
-        "complaint": "Fan not working",
-        "label": "Electricity"
-    },
-    {
-        "type": "easy",
-        "complaint": "Room is dirty",
-        "label": "Cleaning"
-    },
+def get_task():
+    tasks = [
+        {
+            "type": "easy",
+            "complaint": "Light not working",
+            "category": "Electricity",
+            "priority": "Low"
+        },
+        {
+            "type": "medium",
+            "complaint": "Water leakage in bathroom",
+            "category": "Plumbing",
+            "priority": "Medium"
+        },
+        {
+            "type": "hard",
+            "complaint": "Room dirty and fan not working",
+            "category": ["Cleaning", "Electricity"],
+            "priority": "High"
+        }
+    ]
 
-    # MEDIUM
-    {
-        "type": "medium",
-        "complaint": "Room extremely dirty",
-        "label": "Cleaning",
-        "priority": "High"
-    },
-    {
-        "type": "medium",
-        "complaint": "Water leaking in bathroom",
-        "label": "Plumbing",
-        "priority": "Medium"
-    },
-
-    # HARD
-    {
-        "type": "hard",
-        "complaint": "Room dirty and fan not working",
-        "labels": ["Cleaning", "Electricity"],
-        "priority": "Medium"
-    },
-    {
-        "type": "hard",
-        "complaint": "Water flooding and light not working",
-        "labels": ["Plumbing", "Electricity"],
-        "priority": "High"
-    }
-]
+    return random.choice(tasks)
